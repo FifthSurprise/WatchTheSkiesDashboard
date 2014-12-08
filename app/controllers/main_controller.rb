69f17c0cfc6632040508fa4916ecd46c4e1b40b8
Kevin Chang <kevin.w.chang@gmail.com>
2014-12-07 23:29:30 -0500

    Fixed resetting

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,7 +10,22 @@ class MainController < ApplicationController
   end
 
   def reset_game
-    system 'rake db:reset'
+    if Rails.env.production?
+      Game.delete_all
+      TerrorTracker.delete_all
+      g = Game.create()
+      g.next_round = g.created_at + 15*60
+      g.paused = true
+      g.save
+
+      t = TerrorTracker.create()
+      t.description = "Initial setting of Terror level"
+      t.amount = 0
+      t.round = 0
+      t.save
+    else
+      system 'rake db:reset'
+    end
     redirect_to root_path
   end
 
