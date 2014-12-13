5506a6984eced2f4536b478dca967b11cbb4ae16
Kevin Chang <kevin.w.chang@gmail.com>
2014-12-12 21:06:20 -0500

    Fix bug current round reset

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,7 @@ class Game < ActiveRecord::Base
     TerrorTracker.delete_all
     g = Game.create()
     g.control_message = "Welcome to Watch the Skies!"
+    g.current_round = 0
     g.next_round = g.created_at + 15*60
     g.pause_time = g.created_at
     g.paused = true
